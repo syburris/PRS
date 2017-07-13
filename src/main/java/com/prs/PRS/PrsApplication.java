dd82a363c52dfdb54349966c48b16a15230b37ea
stevenburris <stevenburris.jr@gmail.com>
2017-07-12 22:51:57 -0400

    Init

---
+++ src/main/java/com/prs/PRS/PrsApplication.java
@@ -0,0 +1,12 @@
+package com.prs.PRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrsApplication.class, args);
+	}
+}
